d0780b529aba2d5e0ca063b2530a824ffb8210e1
shaileshkumar97 <kumar.shailesh1597@gmail.com>
2022-11-04 13:28:47 +0000

    chore: Added DockerFile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__, url_base_pathname=os.getenv("ROUTE") or "/", external_stylesheets=external_stylesheets)
 
 server = app.server
 
